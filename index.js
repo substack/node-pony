3918863adde186c6ae4b6fe35912c7864514e8c8
James Halliday <mail@substack.net>
2013-11-25 01:52:35 +0800

    remove the seq dependency and the cute overly-clever currying

--- index.js
+++ index.js
@@ -1,105 +1,89 @@
 var smtp = require('smtp-protocol');
-var seq = require('seq');
 var Request = require('./lib/request');
 
 module.exports = function pony (params, cb) {
-    if (cb === undefined) {
-        // curry the parameters
-        return function (params_, cb_) {
-            if (typeof params_ === 'function') {
-                cb_ = params_;
-                params_ = {};
-            }
-            Object.keys(params_).forEach(function (key) {
-                params[key] = params_[key];
-            });
-            
-            return pony(params, cb_);
-        };
-    }
+    var mail, finished = false;
     
-    var finished = false;
-    var c = smtp.connect(params.host, params.port, function (mail) {
-        
-        function error (msg, code, lines) {
-            var s = msg + ': ' + lines.join(' ');
-            var err = new Error(s);
-            err.code = code;
-            err.lines = lines;
-            err.message = s;
-            cb(err);
-            
-            finished = true;
-            mail.quit();
-        }
-        
-        seq()
-            .seq(function () {
-                mail.on('greeting', this.ok);
-            })
-            .seq(function (code, lines) {
-                if (!(code >= 200 && code < 300)) {
-                    error('greeting code not ok', code, lines);
-                }
-                else mail.helo(params.domain || 'localhost', this);
-            })
-            .seq(function (code, lines) {
-                if (!(code >= 200 && code < 300)) {
-                    error('HELO not ok', code, lines);
-                }
-                else mail.from(params.from, this);
-            })
-            .seq(function (code, lines) {
-                if (!(code >= 200 && code < 300)) {
-                    error('FROM not ok', code, lines);
-                }
-                else mail.to(params.to, this);
-            })
-            .seq(function (code, lines) {
-                if (!(code >= 200 && code < 300)) {
-                    error('TO not ok', code, lines);
-                }
-                else mail.data(this);
-            })
-            .seq_(function (next, code, lines) {
-                if (code != 354) {
-                    error('DATA not ok', code, lines);
-                }
-                else {
-                    var req = new Request;
-                    mail.message(req, function (err, code, lines) {
-                        if (err) {
-                            cb(err);
-                            finished = true;
-                            mail.quit();
-                        }
-                        else if (!(code >= 200 && code < 300)) {
-                            req.emit('error', new Error(
-                                'message rejected: '
-                                + code + ' ' + lines.join(' ')
-                            ));
-                        }
-                        else next()
-                    });
-                    cb(null, req);
-                }
-            })
-            .catch(function (err) {
-                cb(err);
-                finished = true;
-                mail.quit();
-            })
-            .seq(function () {
-                mail.quit();
-                finished = true;
-            })
-        ;
+    cb = (function (cb_) {
+        return function () {
+            cb = function () {};
+            return cb_.apply(this, arguments);
+        };
+    })(cb);
+    
+    var c = smtp.connect(params.host, params.port, function (mail_) {
+        mail = mail_;
+        mail.on('greeting', function (code, lines) {
+            if (code < 200 || code >= 300) {
+                error('greeting code not ok', code, lines);
+            }
+            else mail.helo(params.domain || 'localhost', onhelo);
+        });
     });
     
-    c.on('error', cb);
+    c.on('error', function (err) {
+        cb(err);
+        if (mail) mail.quit();
+        cb = function () {};
+    });
+    
     c.on('end', function () {
         if (!finished) cb('connection terminated early');
     });
     
     return c;
+    
+    function error (msg, code, lines) {
+        var s = msg + ': ' + lines.join(' ');
+        var err = new Error(s);
+        err.code = code;
+        err.lines = lines;
+        err.message = s;
+        finished = true;
+        cb(err);
+        if (mail) mail.quit();
+    }
+    
+    function onhelo (err, code, lines) {
+        if (code < 200 || code >= 300) {
+            error('HELO not ok', code, lines);
+        }
+        else mail.from(params.from, onfrom);
+    }
+    
+    function onfrom (err, code, lines) {
+        if (code < 200 || code >= 300) {
+            error('FROM not ok', code, lines);
+        }
+        else mail.to(params.to, onto);
+    }
+    
+    function onto (err, code, lines) {
+        if (code < 200 || code >= 300) {
+            error('TO not ok', code, lines);
+        }
+        else mail.data(ondata)
+    }
+    
+    function ondata (err, code, lines) {
+        if (code != 354) {
+            return error('DATA not ok', code, lines);
+        }
+        var req = new Request;
+        mail.message(req, function (err, code, lines) {
+            finished = true;
+            
+            if (err) return req.emit('error', err);
+            req.emit('code', code, lines);
+            
+            if (code < 200 || code >= 300) {
+                req.emit('error', new Error(
+                    'message rejected: '
+                    + code + ' ' + lines.join(' ')
+                ));
+            }
+            else mail.quit();
+        });
+        cb(null, req);
+    }
 };
